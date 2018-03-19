7e007d337cf854c745abbca2302d1421aed5a4d5
NoahHoi365 <31952674+NoahHoi365@users.noreply.github.com>
2018-03-19 09:10:38 +0100

    Add files via upload

---
+++ form4_err/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Noah Eelman GD1A</h3>
+<hr>
+<pre>
+  <form method="post">
+
+    naam: 	 <input type="text" name="name" id="name" autofocus>
+
+    email: 	  <input type="email" name="email" id="email">
+
+    bericht: 	<input type="text" name="message" id="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
